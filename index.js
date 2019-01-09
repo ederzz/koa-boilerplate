37f947435aa4a6b30c7a38f312f96e6bd75a982d
shenyiling <jzx19951028yyjy@163.com>
2019-01-09 21:41:44 +0800

    fix: error collection

--- index.js
+++ index.js
@@ -21,75 +21,80 @@ const { mimeCollections } =require('./constants')
 
 const app = new Koa();
 
-try {
-    /**指定静态资源目录 */
-    app.use(serve(staticDirPath));
+// error log
+app.use(async (ctx, next) => {
+    try {
+        await next()
+    } catch (err) {
+        const errorLog = `${new Date()} 发生错误:\n${err.stack}\n`;
+        fs.appendFileSync('./log/error.log', errorLog);
+    }
+})
 
-    /**模板引擎 */
-    app.use(nunjucks({
-        ext: 'html',
-        path: path.join(__dirname, 'views'),
-        nunjucksConfig: {
-            trimBlocks: true 
-        }
-    }));
+/**指定静态资源目录 */
+app.use(serve(staticDirPath));
 
-    /**解析post请求 */
-    app.use(bodyParser({
-        formidable:{
-            uploadDir: __dirname + '/static', // directory where files will be uploaded
-            keepExtensions: true,
-            onFileBegin(name, file) {
-                const {
-                    path: filePath,
-                    type
-                } = file
+/**模板引擎 */
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),
+    nunjucksConfig: {
+        trimBlocks: true 
+    }
+}));
 
-                const paths = filePath.split('/')
-                const suffixReg = /^(.*)(\..*)$/
-                const suffix = suffixReg.exec(paths[paths.length -1])[2]
-                let dirName
-                if (mimeCollections.imgType.includes(type)) {
-                    dirName = 'imgs'
-                } else if (mimeCollections.musicType.includes(type)) {
-                    dirName = 'music'
-                } else {
-                    return null
-                }
-                                
-                file.path = path.resolve(paths.slice(0, -1).join('/'), dirName, `${shortid.generate()}${suffix}`)
+/**解析post请求 */
+app.use(bodyParser({
+    formidable:{
+        uploadDir: __dirname + '/static', // directory where files will be uploaded
+        keepExtensions: true,
+        onFileBegin(name, file) {
+            const {
+                path: filePath,
+                type
+            } = file
+
+            const paths = filePath.split('/')
+            const suffixReg = /^(.*)(\..*)$/
+            const suffix = suffixReg.exec(paths[paths.length -1])[2]
+            let dirName
+            if (mimeCollections.imgType.includes(type)) {
+                dirName = 'imgs'
+            } else if (mimeCollections.musicType.includes(type)) {
+                dirName = 'music'
+            } else {
+                return null
             }
-        },
-        multipart: true,
-        urlencoded: true
-    }))
+                            
+            file.path = path.resolve(paths.slice(0, -1).join('/'), dirName, `${shortid.generate()}${suffix}`)
+        }
+    },
+    multipart: true,
+    urlencoded: true
+}))
 
-    /**每次http请求都会通过app.use使用中间件 */
-    app.use(async (ctx, next) => {
-        const sTime = Date.now();
-        await next();
-        
-        const eTime = Date.now();
-        const log = `请求地址：${ctx.path},请求方法：${ctx.request.method},响应时间：${eTime - sTime}ms,响应状态:${ctx.response.status}--请求时间：${new Date()}\n`;
-        console.log(chalk.green(log))
-        fs.appendFileSync('./log/app.log', log);
-    });
+/**每次http请求都会通过app.use使用中间件 */
+app.use(async (ctx, next) => {
+    const sTime = Date.now();
+    await next();
+    
+    const eTime = Date.now();
+    const log = `请求地址：${ctx.path},请求方法：${ctx.request.method},响应时间：${eTime - sTime}ms,响应状态:${ctx.response.status}--请求时间：${new Date()}\n`;
+    console.log(chalk.green(log))
+    fs.appendFileSync('./log/app.log', log);
+});
 
-    // cors config
-    app.use(cors())
+// cors config
+app.use(cors())
 
-    /**路由 */
-    app.use(indexRouter.routes())
-    app.use(accountRouter.routes())
-    app.use(apiTestRouter.routes())
-    app.use(githubApiRouter.routes())
-    app.use(uploadRouter.routes())
+/**路由 */
+app.use(indexRouter.routes())
+app.use(accountRouter.routes())
+app.use(apiTestRouter.routes())
+app.use(githubApiRouter.routes())
+app.use(uploadRouter.routes())
 
-    app.listen(port, () => {
-        console.log(chalk.green(`server is running at ${hostname}:${port}`));
-        console.log(process.env.NODE_ENV);
-    });
-} catch(err) {
-    const errorLog = `${err.name},${err.message},${err.stack},${new Date()}`;
-    fs.appendFileSync('./log/error.log', errorLog);
-}
+app.listen(port, () => {
+    console.log(chalk.green(`server is running at ${hostname}:${port}`));
+    console.log(process.env.NODE_ENV);
+});
